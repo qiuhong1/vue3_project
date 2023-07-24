4d75b207aadb2a1633c31af5eae6b1d523b04121
qiuhong <786843361@qq.com>
2023-07-24 16:46:48 +0800

    提交代码

--- src/request/api.js
+++ src/request/api.js
@@ -1,4 +1,6 @@
 import { get, post, getDynamicynamic, getFileUseBlobByPost } from './http'
 
 // export const getData = (p) => get('/home/category/head', p)
-export const getCategoryAPI = (p) => get('/home/category/head', p)//获取分类
+export const getCategoryAPI = (p) => get('/home/category/head', p) //获取分类
+export const getBannerAPI = (p) => get('/home/banner', p) //获取banner
+export const getnewAPI = (p) => get('/home/new', p) //获取banner
---
+++ src/views/Home/components/HomeHot.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import HomePanel from './HomePanel.vue'
+import { getnewAPI } from '@/request/api.js'
+
+let newList = ref([])
+onMounted(() => {
+  getnewAPI().then((res) => {
+    console.log(res)
+    newList.value = res.result
+  })
+})
+</script>
+
+<template>
+  <HomePanel title="人气推荐" subTitle="人气爆款 不容错过">
+    <ul class="goods-list">
+      <li v-for="item in newList" :key="item.id">
+        <RouterLink to="/">
+          <img :src="item.picture" alt="" />
+          <p class="name">{{ item.name }}</p>
+          <p class="price">&yen;{{ item.price }}</p>
+        </RouterLink>
+      </li>
+    </ul>
+  </HomePanel>
+</template>
+
+<style scoped lang="scss">
+.goods-list {
+  display: flex;
+  justify-content: space-between;
+  height: 406px;
+
+  li {
+    width: 306px;
+    height: 406px;
+
+    background: #f0f9f4;
+    transition: all 0.5s;
+
+    &:hover {
+      transform: translate3d(0, -3px, 0);
+      box-shadow: 0 3px 8px rgb(0 0 0 / 20%);
+    }
+
+    img {
+      width: 306px;
+      height: 306px;
+    }
+
+    p {
+      font-size: 22px;
+      padding-top: 12px;
+      text-align: center;
+      text-overflow: ellipsis;
+      overflow: hidden;
+      white-space: nowrap;
+    }
+
+    .price {
+      color: $priceColor;
+    }
+  }
+}
+</style>
